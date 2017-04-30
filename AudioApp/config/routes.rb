bd634aca47aab7fd9bc9ade59760334592f02170
fimac <fimccawley@gmail.com>
2017-04-30 21:24:17 +1000

    Created controllers for pages, engineers and artists. Created html.erb pages for index, show, new, edit, home and about

--- AudioApp/config/routes.rb
+++ AudioApp/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'engineers/index'
+
+  get 'engineers/show'
+
+  get 'engineers/new'
+
+  get 'engineers/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
