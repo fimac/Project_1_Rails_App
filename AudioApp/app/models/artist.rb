7160cbf279412ce02897c123f13165cc6f0c4086
fimac <fimccawley@gmail.com>
2017-05-01 22:57:59 +1000

    updated associations between users and artists/enginers. Commit prior to migrating additional columns for fk user_id into artists and engineers

--- AudioApp/app/models/artist.rb
+++ AudioApp/app/models/artist.rb
@@ -2,4 +2,5 @@ class Artist < ApplicationRecord
   has_many :bookings
   has_many :engineers, :through => :bookings
 
+  belongs_to :user, optional: true
 end
--- AudioApp/app/models/engineer.rb
+++ AudioApp/app/models/engineer.rb
@@ -1,4 +1,6 @@
 class Engineer < ApplicationRecord
   has_many :bookings
   has_many :artists, :through => :bookings
+
+  belongs_to :user, optional: true
 end
