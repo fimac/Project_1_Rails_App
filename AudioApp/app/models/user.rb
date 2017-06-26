7628aa3ecac56be6b079ef11d53dc2908fea2a93
fimac <fimccawley@gmail.com>
2017-06-26 17:43:41 +1000

    updated css

--- AudioApp/app/models/user.rb
+++ AudioApp/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+
   has_many :artists
   has_many :engineers
 # Added the below so I can access the bookings a user has through their engineer/artist profiles
